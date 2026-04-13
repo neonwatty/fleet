6041a9129d0379d2446f177712fcde1d4feac18f
neonwatty <jermwatt@gmail.com>
2026-04-12 18:05:41 -0700

    feat(menubar): add app entry point and empty AppDelegate

---
+++ menubar/Sources/AppDelegate.swift
@@ -0,0 +1,8 @@
+import AppKit
+
+@MainActor
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Wired in Task 7 once StatusItemController exists.
+    }
+}
